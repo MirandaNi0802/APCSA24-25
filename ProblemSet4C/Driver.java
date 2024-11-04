f13e37ccba7c0d01a875fbae9cdbf114d093afda
MirandaNi0802 <139798558+MirandaNi0802@users.noreply.github.com>
2024-11-04 12:22:18 -0800

    Add files via upload

---
+++ ProblemSet4C/Driver.java
@@ -0,0 +1,6 @@
+public class Driver{
+    public static void main(String[] args){
+        System.out.println(Review.totalSentiment("SimpleReview.txt"));
+        System.out.println(Review.fakeReview("SimpleReview.txt"));
+    }
+}
